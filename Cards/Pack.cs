dd52956bd36de4de20973d9e72d80eb2b53dc85b
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-09 18:27:25 -0400

    Pack() and DealCardFrom Pack() methods are completed

--- Cards/Pack.cs
+++ Cards/Pack.cs
@@ -11,13 +11,33 @@ namespace Cards
 
         public Pack()
         {
-            // TODO: initialize the pack of cards
+            // initialize the pack of cards
+            this.cardPack = new PlayingCard[NumSuits, CardsPerSuit];
+            for (Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++)
+            {
+                for (Value value = Value.Two; value <= Value.Ace; value++)
+                {
+                    this.cardPack[(int)suit, (int)value] = new PlayingCard(suit, value);
+                }
+            }
         }
 
         public PlayingCard DealCardFromPack()
         {
-            // TODO: pick a random card, remove it from the pack, and return it
-            throw new NotImplementedException("DealCardFromPack - TBD");
+            // pick a random card, remove it from the pack, and return it
+            Suit suit = (Suit)randomCardSelector.Next(NumSuits);
+            while (this.IsSuitEmpty(suit))
+            {
+                suit = (Suit)randomCardSelector.Next(NumSuits);
+            }
+            Value value = (Value)randomCardSelector.Next(CardsPerSuit);
+            while (this.IsCardAlreadyDealt(suit, value))
+            {
+                value = (Value)randomCardSelector.Next(CardsPerSuit);
+            }
+            PlayingCard card = this.cardPack[(int)suit, (int)value];
+            this.cardPack[(int)suit, (int)value] = null;
+            return card;
         }
 
         private bool IsSuitEmpty(Suit suit)
